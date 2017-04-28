41306477002905b313e14d08b6c555edca274ec1
Matteo Ferrando <matteo.ferrando2@gmail.com>
2017-04-28 11:50:19 -0400

    added ramdisk_up_pg and ramdisk_down_pg to manage clusters

---
+++ fish/config/custom/ramdisk_down_pg.fish
@@ -0,0 +1,7 @@
+function ramdisk_down_pg -d "description"
+    if set -Uq RAMDISK_CLUSTER_DIR
+      pg_ctl stop -D $RAMDISK_CLUSTER_DIR
+      set -eU RAMDISK_CLUSTER_DIR
+    end
+    ramdisk_down
+end
