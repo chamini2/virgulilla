687e17713687459b6d8c34431c4a0c0b484b5e9b
Matteo Ferrando <matteo.ferrando2@gmail.com>
2017-08-12 17:50:24 -0430

    testing if rvm is present before setting it up

--- fish/config/source/load.fish
+++ fish/config/source/load.fish
@@ -6,8 +6,10 @@ if not test $NVM_BIN
 end
 
 # load the default ruby to use with rvm
-echo -n "Loading rvm "
-rvm use default > /dev/null 2> /dev/null
-rvm current
+if type -q rvm
+  echo -n "Loading rvm "
+  rvm use default > /dev/null 2> /dev/null
+  rvm current
+end
 
 set fish_function_path $fish_config_path/custom $fish_function_path
