e1e73bbe6f80c2dcd9269ad05f40524b68410a88
Matteo Ferrando <matteo.ferrando2@gmail.com>
2018-03-24 18:09:10 -0400

    add stack binaries to path

--- fish/env.fish
+++ fish/env.fish
@@ -3,6 +3,8 @@ set -U fish_user_paths /usr/local/bin/ $fish_user_paths
 
 ## Cabal and sandboxes
 set -U fish_user_paths $HOME/.cabal/bin .cabal-sandbox/bin $fish_user_paths
+## Stack
+set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
 ## npm
 set -U fish_user_paths node_modules/.bin $fish_user_paths
