92d41d113ed319b7a48cba0b607f14660caabffc
Matteo Ferrando <matteo.ferrando2@gmail.com>
2018-04-14 12:02:40 -0400

    use `status filenane` instead of find for cuetag.sh

--- fish/config/custom/cuetag.fish
+++ fish/config/custom/cuetag.fish
@@ -1,5 +1,5 @@
 function cuetag
   set -l escaped_argv (string escape $argv)
-  set -l cuetag_path (find -L $fish_function_path -name cuetag.sh -type f 2>/dev/null)
+  set -l cuetag_path (dirname (status filename))/cuetag.sh
   bash $cuetag_path $escaped_argv
 end
