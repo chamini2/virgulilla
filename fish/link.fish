d280b7eb6bcf197fa5a4275c5ebf34175054e0b6
Matteo Ferrando <matteo.ferrando2@gmail.com>
2018-07-30 12:50:50 -0400

    fix link script for Ubuntu machine

--- fish/link.fish
+++ fish/link.fish
@@ -1,11 +1,14 @@
 set -l fish_config_path $HOME/.config/fish
 
 mkdir -p $fish_config_path
-ln -fs "(pwd)/config/*" $fish_config_path/
+for dst in config/*
+    set -l file $PWD/$dst
+    ln -fs $file $fish_config_path/
+end
 
 if not functions --query fisher
-  # fisherman (https://github.com/fisherman/fisherman)
-  curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+    # fisherman (https://github.com/fisherman/fisherman)
+    curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 end
 
 # install plugins in fishfile
