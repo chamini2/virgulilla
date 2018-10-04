b08ca5a73fddaf2c29b2d1ae3b6edc65b51c27da
Matteo Ferrando <matteo.ferrando2@gmail.com>
2018-10-04 17:20:09 -0400

    fix kubegetpod function

--- fish/config/custom/kubegetpod.fish
+++ fish/config/custom/kubegetpod.fish
@@ -2,5 +2,5 @@ function kubegetpod
     if test (count $argv) -ne 1
         return
     end
-    kubectl get pods | grep -Eo "$argv[1]-[0-9a-z]+-[0-9a-z]+\b"
+    kubectl get pods | grep -Eo "$argv[1]-[0-9a-z]+-[0-9a-z]+ " | string trim
 end
