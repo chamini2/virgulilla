a7b4d6b56c6aa2365fc403b071becbd8cbb143e1
Matteo Ferrando <matteo.ferrando2@gmail.com>
2021-10-22 20:45:15 -0400

    refactoring a bit

--- .config/fish/source/load.fish
+++ .config/fish/source/load.fish
@@ -1,4 +1,18 @@
-# load the default ruby to use with rvm
-if type -q rvm
-  rvm default
+if status --is-interactive
+
+  # load the default ruby to use with rvm
+  if type -q rvm
+    rvm default
+  end
+
+  # load jenv
+  if type -q jenv
+    jenv init - > /dev/null
+  end
+
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
+
+  and pyenv init --path | source
+  pyenv init - | source
+
 end
--- .config/fish/startup/env.fish
+++ .config/fish/startup/env.fish
@@ -1,29 +1,34 @@
 # Reset fish_user_paths
 set -U fish_user_paths
 
-function add_if_dir
+function add_dir_to_path
+    set -U fish_user_paths $argv[1] $fish_user_paths
+end
+
+function add_if_dir_to_path
     if test -d $argv[1]
-        set -U fish_user_paths $argv[1] $fish_user_paths
+        add_dir_to_path $argv[1]
     end
 end
 
 ## Home
-add_if_dir $HOME/bin
+add_if_dir_to_path $HOME/bin
 
 ## Homebrew
-add_if_dir /usr/local/sbin
-add_if_dir /usr/local/bin
+add_dir_to_path /usr/local/sbin
+add_dir_to_path /usr/local/bin
 
 ## Cabal and sandboxes
-add_if_dir $HOME/.cabal/bin
-add_if_dir .cabal-sandbox/bin
+add_if_dir_to_path $HOME/.cabal/bin
+add_dir_to_path .cabal-sandbox/bin
 ## Stack
-add_if_dir $HOME/.local/bin
+add_if_dir_to_path $HOME/.local/bin
 
 ## npm
-add_if_dir node_modules/.bin
+add_dir_to_path node_modules/.bin
 
 ## Kubectl
 set -Ux KUBECONFIG kubeconfig
 
-functions -e add_if_dir
+functions -e add_if_dir_to_path
+functions -e add_dir_to_path
