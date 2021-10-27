12f9243d0f440753bb15317e5d22349b81309ecc
Matteo Ferrando <matteo.ferrando2@gmail.com>
2021-10-27 13:08:03 -0400

    Update startup

--- .config/fish/startup/abbr.fish
+++ .config/fish/startup/abbr.fish
@@ -1,8 +1,12 @@
-abbr -- - 'cd -'
-abbr awake caffeinate
-abbr su 'sudo su'
-abbr sl ls
-abbr gut git
-abbr wow 'git status'
-abbr gap 'git add -p'
-abbr gmn 'git merge --no-ff'
+abbr -a -U -- - 'cd -'
+abbr -a -U -- awake caffeinate
+abbr -a -U -- gap 'git add -p'
+abbr -a -U -- gcm 'git commit -m'
+abbr -a -U -- gd 'git diff'
+abbr -a -U -- gds 'git diff --staged'
+abbr -a -U -- gm 'git merge'
+abbr -a -U -- gmn 'git merge --no-ff'
+abbr -a -U -- gut git
+abbr -a -U -- sl ls
+abbr -a -U -- su 'sudo su'
+abbr -a -U -- wow 'git status'
--- .config/fish/startup/env.fish
+++ .config/fish/startup/env.fish
@@ -1,34 +1,31 @@
 # Reset fish_user_paths
 set -U fish_user_paths
 
-function add_dir_to_path
-    set -U fish_user_paths $argv[1] $fish_user_paths
-end
-
-function add_if_dir_to_path
-    if test -d $argv[1]
-        add_dir_to_path $argv[1]
-    end
-end
-
 ## Home
-add_if_dir_to_path $HOME/bin
+fish_add_path $HOME/bin
 
 ## Homebrew
-add_dir_to_path /usr/local/sbin
-add_dir_to_path /usr/local/bin
+fish_add_path /usr/local/sbin
+fish_add_path /usr/local/bin
 
 ## Cabal and sandboxes
-add_if_dir_to_path $HOME/.cabal/bin
-add_dir_to_path .cabal-sandbox/bin
+fish_add_path $HOME/.cabal/bin
+fish_add_path ./.cabal-sandbox/bin
 ## Stack
-add_if_dir_to_path $HOME/.local/bin
+fish_add_path $HOME/.local/bin
 
 ## npm
-add_dir_to_path node_modules/.bin
+fish_add_path ./node_modules/.bin
+
+# Python
+fish_add_path $HOME/.pyenv/bin
+fish_add_path $HOME/.poetry/bin
+
+# Java
+fish_add_path $HOME/.jenv/bin
+
+# Go
+fish_add_path $HOME/.go/bin
 
 ## Kubectl
 set -Ux KUBECONFIG kubeconfig
-
-functions -e add_if_dir_to_path
-functions -e add_dir_to_path
