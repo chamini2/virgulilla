4865fa599beeb9fa5b9f71bffbda6401bb20ee12
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-09-29 11:09:36 -0400

    small fixes to existing functions

--- .config/fish/custom/desktoptoggle.fish
+++ .config/fish/custom/desktoptoggle.fish
@@ -1,6 +1,12 @@
 function desktoptoggle
     set -l current (defaults read com.apple.finder CreateDesktop)
+    if test -z $current
+        echo "desktoptoggle: error reading current configuration"
+        return 1
+    end
+
     set -l new (if test $current -eq 1; echo 0; else; echo 1; end)
+
     defaults write com.apple.finder CreateDesktop $new
     killall Finder
 end
--- .config/fish/custom/mkd.fish
+++ .config/fish/custom/mkd.fish
@@ -1,3 +1,12 @@
-function mkd -d "mkdir and cd into it"
-    mkdir -p $argv ;and cd $argv
+function mkd -d "Create a directory and set CWD"
+    command mkdir $argv
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
 end
